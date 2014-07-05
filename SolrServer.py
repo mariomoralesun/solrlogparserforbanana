e53f4dc7f12de558d2ec421603cd84b1610cacbc
wrdrvr <wrdrvr@users.noreply.github.com>
2014-07-05 14:38:51 -0400

    Create SolrServer.py

---
+++ SolrServer.py
@@ -0,0 +1,67 @@
+import re
+import json
+import requests
+import argparse
+import os
+import datetime
+import atexit
+
+class SolrServer:
+    databuffer = ''
+    buffercount = 0
+    sendinc = 1000
+    
+    def __init__(self,solrserver,sendinc):
+        self.solr = solrserver
+        
+        if solrserver[-1] == '/':
+            self.solr = solrserver
+        else:
+            self.solr = solrserver + '/'
+        
+        self.sendinc = sendinc
+        
+    def set_param(p,v):
+        self[p] = v
+            
+    def log_out(self,s):
+        print("{} - {}".format(datetime.datetime.now(),s)) 
+    
+    def send_dict_to_solr(self,data,commit):
+        data = json.dumps(data)
+        if self.buffercount == 0:
+            self.databuffer = '[\n'
+        self.buffercount += 1
+        self.databuffer += data + ',\n'
+        
+        if self.buffercount >= self.sendinc:
+            if commit == 1 or commit == True:
+                solr = self.solr+'update?commit=true'
+                self.log_out("Sending Commit with next batch")
+            else:
+                solr = self.solr+'update'
+        
+            self.databuffer = self.databuffer[:-2]
+            self.databuffer += ']\n'
+            
+            r = requests.post(solr,data=self.databuffer,headers = {'content-type': 'application/json'})
+            if r.status_code != 200:
+                self.log_out("ERROR - Couldn't Index Data Into Solr")
+                self.log_out(r.status_code)
+                self.log_out(r.raw)
+                self.log_out(r.url)
+                with open('error.log','w+') as er:
+                    er.write(data)
+                    er.close()
+            else:
+                self.log_out("Send Successful")
+            r.close()
+            self.databuffer = ''
+            self.buffercount = 0
+        return True
+    
+    def send_rest(self):
+        self.buffercount = self.sendinc
+        t = {}
+        t['id'] = 'commit'
+        self.send_dict_to_solr(t,1)
